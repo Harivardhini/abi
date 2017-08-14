af90caf7493f9d588eff6b19786712318d1b66b8
Harivardhini <30343743+Harivardhini@users.noreply.github.com>
2017-08-14 17:10:02 +0530

    Create numbu.c

---
+++ numbu.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(0
+{
+int number,i=1,sum=0;
+printf("\n please enter any integer value\n");
+scanf("%d",&number);
+while(i<=number)
+{
+sum=sum +i;
+i++;
+}
+printf("sum of natural numbers =%d",sum);
+return 0;
+}
